e17adccb6e2de108762b5ffab83c8f5cb28cc225
xxxycl <xxxycl.com@qq.com>
2021-11-04 11:06:34 +0800

    Update dio_proxy.dart
    直接是否启用代理，默认不启用

--- lib/dio_proxy.dart
+++ lib/dio_proxy.dart
@@ -7,20 +7,27 @@ import 'package:dio/adapter.dart';
 /// Dio HttpProxy Adapter 给通过网关的请求加上签名
 /// [ipAddr] ip地址，默认是localhost
 /// [port] 端口，默认是8888
+/// [proxyIsEnable] 代理是否启用，默认是false，不启用
 ///
 class HttpProxyAdapter extends DefaultHttpClientAdapter {
   final String ipAddr;
   final int port;
+  final bool proxyIsEnable;
 
-  HttpProxyAdapter({this.ipAddr = 'localhost', this.port = 8888}) {
-    onHttpClientCreate = (client) {
-      var proxy = '$ipAddr:$port';
-      client.findProxy = (url) {
-        return 'PROXY $proxy';
+  HttpProxyAdapter(
+      {this.ipAddr = 'localhost',
+      this.port = 8888,
+      this.proxyIsEnable = false}) {
+    if (proxyIsEnable == true) {
+      onHttpClientCreate = (client) {
+        var proxy = '$ipAddr:$port';
+        client.findProxy = (url) {
+          return 'PROXY $proxy';
+        };
+
+        client.badCertificateCallback =
+            (X509Certificate cert, String host, int port) => true;
       };
-
-      client.badCertificateCallback =
-          (X509Certificate cert, String host, int port) => true;
-    };
+    }
   }
 }
